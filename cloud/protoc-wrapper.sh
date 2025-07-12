67fb732a10d4b14e470fbe8480a58ff74b81f060
Kerigan <kerigancreighton@gmail.com>
2025-07-12 10:26:47 -0500

    Use correct UPX executable

--- cloud/protoc-wrapper.sh
+++ cloud/protoc-wrapper.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# usage: ./cloud/protoc-wrapper.sh <path/to/protoc> <args>
+
 SCRIPT_PATH="$(dirname $([ -L $0 ] && echo "$(dirname $0)/$(readlink -n $0)" || echo $0))"
 
 TOPLEVEL="$(cd "${SCRIPT_PATH}/.." && pwd)"
