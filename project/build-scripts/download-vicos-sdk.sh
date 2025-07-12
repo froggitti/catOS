9650c26fe0ea7a717824f84da00ad6aa2fa36120
Kerigan <kerigancreighton@gmail.com>
2025-07-12 13:50:05 -0500

    Patch vicos-sdk to include libtinfo5

--- project/build-scripts/download-vicos-sdk.sh
+++ project/build-scripts/download-vicos-sdk.sh
@@ -2,6 +2,9 @@
 
 TOOLCHAIN_VERSION_DEFAULT="5.2.1-r06"
 
+cd ../../..
+ORIGDIR="$(pwd)"
+
 if [[ $1 != "" ]]; then
     TOOLCHAIN_VERSION="$1"
 else
@@ -12,10 +15,12 @@ ADEPS="$HOME/.anki"
 
 if [[ "$(uname -a)" == *"x86_64"* && "$(uname -a)" == *"Linux"* ]]; then
     HOST="amd64-linux"
+    TINFO_HOST="amd64"
     elif [[ "$(uname -a)" == *"arm64"* && "$(uname -a)" == *"Darwin"* ]]; then
     HOST="arm64-macos"
     elif [[ "$(uname -a)" == *"aarch64"* && "$(uname -a)" == *"Linux"* ]]; then
     HOST="arm64-linux"
+    TINFO_HOST="arm64"
 else
     echo "This can only be run on x86_64 Linux, arm64 Linux, or arm64 macOS systems at the moment."
     echo "This will be fixed once I compile the new toolchain for more platforms."
@@ -32,4 +37,9 @@ if [[ ! -d "$ADEPS/vicos-sdk/dist/${TOOLCHAIN_VERSION}/prebuilt" ]]; then
 	echo "Extracting vicos-sdk ${TOOLCHAIN_VERSION}..."
     tar -zxf vicos-sdk_${TOOLCHAIN_VERSION}_$HOST.tar.gz
     rm -f vicos-sdk_${TOOLCHAIN_VERSION}_$HOST.tar.gz
+fi
+
+if [[ ! -f "$ADEPS/vicos-sdk/dist/${TOOLCHAIN_VERSION}/prebuilt/lib/libtinfo.so.5" ]] && [[ "$(uname -a)" == *"Linux"* ]]; then
+    echo "Patching compiler to include libtinfo5"
+    cp -r "$ORIGDIR/3rd/libtinfo5/$TINFO_HOST/libtinfo.so.5.9" "$ADEPS/vicos-sdk/dist/${TOOLCHAIN_VERSION}/prebuilt/lib/libtinfo.so.5"
 fi
