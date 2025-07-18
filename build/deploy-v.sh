7c133ce301fca1bbc636b598df68faa5a94ade75
Kerigan <kerigancreighton@gmail.com>
2025-07-17 22:25:26 -0500

    Update README, include setenv script based on usefulALiases.sh, make deploy script and alias ask for IP if it doesn't exist

--- build/deploy-v.sh
+++ build/deploy-v.sh
@@ -4,15 +4,22 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 echo $DIR
 cd $DIR
 
-if [[ ! -f robot_ip.txt ]]; then
-    echo "You must echo your robot's IP to robot_ip.txt in the root of this repo before running this script."
-    exit 1
-fi
+function IS_ROBOT_IP_THERE() {
+    if [[ ! -f robot_ip.txt ]]; then
+        echo "Robot IP file doesn't exist. What is your robot's IP?"
+        echo -n "(XXX.XXX.XXX.XXX): "
+        read ipaddr
+        echo "$ipaddr" > robot_ip.txt
+    fi
+}
+
+IS_ROBOT_IP_THERE
 
 if [[ ! -f robot_sshkey ]]; then
     echo "You must copy your robot's SSH key the root of this repo with the name robot_sshkey before running this script."
     exit 1
 fi
+
 if [[ "$(uname -a)" == *"Darwin"* ]]; then
     ssh-add robot_sshkey && \
     ./project/victor/scripts/victor_deploy.sh -c Release -b && \
